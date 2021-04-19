ee80c012c43801c2df2a0106a335e910c65b3cf6
mohammadnedaei <nedaei2002@gmail.com>
2021-04-19 19:33:51 +0430

    Added TypeWriter to js File

--- js/main.js
+++ js/main.js
@@ -51,6 +51,7 @@
 *                            20. Contact Form
 * 		                     21. Triggered CSS 3 Plugin
 * 			                 22. Jarallax
+*                            23. TypeWriter
 * ==============================================================================================
 */
 
@@ -1126,4 +1127,59 @@ function init_jarallax() {
       speed: 0.2
     });
   }
+}
+
+/*
+* =======================================
+!           23. TypeWriter
+* =======================================
+*/
+
+class TypeWriter {
+  constructor(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = "";
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+  }
+
+  type() {
+    if (this.wordIndex > this.words.length) {
+      this.wordIndex = 1;
+    }
+    const current = this.wordIndex % this.words.length;
+    const fullTxt = this.words[current];
+
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+    this.txtElement.innerHTML = <span class="txt">${this.txt}</span>;
+    let typeSpeed = 150;
+    if (this.isDeleting) {
+      typeSpeed /= 2;
+    }
+    if (!this.isDeleting && this.txt === fullTxt) {
+      typeSpeed = this.wait;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+      this.isDeleting = false;
+      this.wordIndex++;
+      typeSpeed = 150;
+    }
+    setTimeout(() => this.type(), typeSpeed);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  const txtElement = document.querySelector(".txt-type");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
+  new TypeWriter(txtElement, words, wait);
 }
